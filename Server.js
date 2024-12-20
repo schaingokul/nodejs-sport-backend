737fa4dd0a0dc463176c5353d12efcc7cd0a3977
schaingokul <schaingokul@gmail.com>
2024-12-20 18:55:59 +0530

    0.0.3.6

--- Server.js
+++ Server.js
@@ -113,7 +113,7 @@ app.get("/", (req,res) => {
 app.listen(PORT, HOST.replace("http://", ""), async () => {
     try {
         await connectDB();
-        console.log(`Server is running on ${IP}`);
+        console.log(`Server is running on ${PORT}`);
     } catch (error) {
         console.log(`Server failed to connect to database: ${error.message}`);
     }
